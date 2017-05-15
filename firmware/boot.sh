a198abaf025221e41dcbf8b95a812a93fb4f0afb
Speccery <speccery@gmail.com>
2017-05-16 00:07:15 +0300

    Resynthetised the TMS99105 version

--- firmware/boot.sh
+++ firmware/boot.sh
@@ -1,7 +1,7 @@
 # boot.sh
 # EP 2016-11-13
-../memloader/memloader 100008 cpu_reset_on.bin
-../memloader/memloader 0 boot99105_0000.bin
+../memloader/memloader -5 100008 cpu_reset_on.bin
+../memloader/memloader -5 BA000 boot99105_0000.bin
 # CPU out of reset
-../memloader/memloader 100008 cpu_reset_off.bin
-../memloader/memloader -k
+../memloader/memloader -5 100008 cpu_reset_off.bin
+../memloader/memloader -5 -k
